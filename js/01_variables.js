f92a5ca5d86a621bba5d4a58d93d07c3fb9007c5
nitin-pal <nitin.pal447053@gmail.com>
2025-06-17 06:34:18 +0000

    datatypes

---
+++ js/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 144556
+let accountEmail = "nitin.pal@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+
+// accountID = 2
+accountEmail = "nitin.pal3435@gamil.com"
+accountPassword = "9876"
+accountCity = "Uttar pardesh"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
+ console.log(accountID)
---
+++ js/02_datatypes.js
@@ -0,0 +1,6 @@
+let name = "Nitin"
+let age = 21
+let isLogin = true
+
+console.log(typeof(isLogin))
+console.log();
