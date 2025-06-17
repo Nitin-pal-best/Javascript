a8d831ffa8d97742c0d1fba5b81f2f573e553bc5
nitin-pal <nitin.pal447053@gmail.com>
2025-06-17 07:37:37 +0000

    Conversion of datatypes

--- js/02_datatypes.js
+++ js/02_datatypes.js
@@ -3,4 +3,16 @@ let age = 21
 let isLogin = true
 
 console.log(typeof(isLogin))
-console.log();
+
+// /*
+// | Type      | Example             |
+// | --------- | ------------------- |
+// | String    | `"Nitin"`           |
+// | Number    | `20`, `3.14`        |
+// | Boolean   | `true`, `false`     |
+// | Undefined | `let x;`            |
+// | Null      | `let x = null;`     |
+// | Object    | `{ name: "Nitin" }` |
+// | Array     | `[1, 2, 3]`         |
+
+// */
---
+++ js/03_conversionOpertion.js
@@ -0,0 +1,40 @@
+// let score = null
+// let score = undefined
+// let score = "Nitin"
+// let score = true
+// let score = false
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valuNumber = Number(score)
+console.log(valuNumber);
+console.log(typeof(valuNumber));
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1
+// false => 0
+
+// let isLogIn = 1 // output: true
+// let isLogIn = 0// output: false
+// let isLogIn = '' //   output: false
+let isLogIn = 'Nitin' // output: true
+
+let booleanLoggedIn = Boolean(isLogIn)
+console.log(booleanLoggedIn);
+
+
+let sumNumber = 33.44
+
+let strNumber = Number(sumNumber)
+console.log(strNumber)
+console.log(typeof (strNumber))
+
+// let sumNumber = 33
+
+// let strNumber = parseFloat(sumNumber)
+// console.log(strNumber)
+// console.log(typeof (strNumber))
