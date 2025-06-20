c96b56af09ebb2d5482868ea79420d367a328526
nitin-pal <nitin.pal447053@gmail.com>
2025-06-20 06:24:06 +0000

    math learning

---
+++ js/07_math.js
@@ -0,0 +1,16 @@
+/*
+console.log(Math.abs(-4.9)) // negative (-) htata hai
+console.log(Math.ceil(4.1)); // koi value decimals me aati hai ek bada dega 4 hai 5 kar dega
+console.log(Math.round(4.4));// koi decimals 4.4 se uper jata hai to ek value bada dega 4 se 5 kar deha 
+console.log(Math.floor(4.6));
+console.log(Math.random(10) * 10);
+*/
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+
---
+++ partices/03_math.js
@@ -0,0 +1,5 @@
+const num = 7.9
+console.log(Math.round(num));
+
+const floo = 9.1
+console.log(Math.floor(floo));
